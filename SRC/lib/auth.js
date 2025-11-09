b2132983551688b876eabbd02ffd08236ef04c94
futureme071396-sketch <futureme071396@gmail.com>
2025-11-09 23:02:06 +0300

    Update auth.js

--- SRC/lib/auth.js
+++ SRC/lib/auth.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "eduteach-hub.firebaseapp.com",
-  projectId: "eduteach-hub",
-  storageBucket: "eduteach-hub.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+  apiKey: "AIzaSyAMirosxN-_81B4te272HvayuVK63QRAGg",
+  authDomain: "mtopanga-secondary-school.firebaseapp.com",
+  projectId: "mtopanga-secondary-school",
+  storageBucket: "mtopanga-secondary-school.firebasestorage.app",
+  messagingSenderId: "783437006663",
+  appId: "1:783437006663:web:6d61c2f022f4073bfb6e62"
 };
 
 const app = initializeApp(firebaseConfig);
